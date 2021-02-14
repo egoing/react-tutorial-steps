egoing/react-sample

step9-ajax - ajax 기능 추가

src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import Article from "./components/Article";
 import Header from "./components/Header";
@@ -7,38 +7,78 @@ import Create from "./components/Create";
 import Update from "./components/Update";
 import Control from "./components/Control";
 
+var URL = process.env.REACT_APP_API_URL;
+if(URL === undefined){
+  console.error('/.env.development.template => /.env.development')
+}
+function getTopics(setTopics) {
+  fetch(URL + "topics")
+    .then(function(type) {
+      return type.json();
+    })
+    .then(function(result) {
+      setTopics(result);
+    });
+}
+
+function Read(props) {
+  var [topic, setTopic] = useState();
+  useEffect(
+    function() {
+      if (props.id === null) return;
+      fetch(URL + "topics/" + props.id)
+        .then(function(type) {
+          return type.json();
+        })
+        .then(function(result) {
+          setTopic(result);
+        });
+    },
+    [props.id]
+  );
+  if (topic) {
+    return <Article title={topic.title} description={topic.description} />;
+  } else {
+    return <>Loading...</>;
+  }
+}
 export default function App() {
   var [mode, setMode] = useState("WELCOME");
-  var [selectedId, setSelectedId] = useState(1);
+  var [selectedId, setSelectedId] = useState(null);
+  var [topic, setTopic] = useState();
   var [nextId, setNextId] = useState(4);
-  var [topics, setTopics] = useState([
-    { id: 1, title: "html", description: "html is..." },
-    { id: 2, title: "css", description: "css is..." },
-    { id: 3, title: "js", description: "js is..." }
-  ]);
+  var [topics, setTopics] = useState([]);
+  useEffect(function() {
+    getTopics(setTopics);
+  }, []);
+
   var article = null;
   if (mode === "WELCOME") {
     article = <Article title="Welcome" description="Hello, WEB" />;
   } else if (mode === "READ") {
-    for (var i = 0; i < topics.length; i++) {
-      if (topics[i].id === selectedId) {
-        article = (
-          <Article
-            title={topics[i].title}
-            description={topics[i].description}
-          />
-        );
-      }
-    }
+    article = <Read id={selectedId} />;
   } else if (mode === "CREATE") {
     article = (
       <Create
         onCreate={function(data) {
-          data.id = nextId;
-          setTopics([...topics, data]);
-          setSelectedId(nextId);
-          setMode("READ");
-          setNextId(nextId + 1);
+          fetch(URL + "topics", {
+            method: "POST",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopics(setTopics);
+              setSelectedId(result.id);
+              setMode("READ");
+            });
         }}
       />
     );
@@ -54,6 +94,25 @@ export default function App() {
       <Update
         data={selectedTopic}
         onUpdate={function(data) {
+          fetch(URL + "topics/" + data.id, {
+            method: "PUST",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopic(setTopics);
+              setSelectedId(result.id);
+              setMode("READ");
+            });
+
           var newTopics = [...topics];
           for (var i = 0; i < newTopics.length; i++) {
             if (newTopics[i].id === data.id) {
@@ -94,7 +153,6 @@ export default function App() {
                 newTopics.push(topics[i]);
               }
             }
-            console.log(newTopics);
             setTopics(newTopics);
             mode = "WELCOME";
           }

egoing/react-tutorial-steps

src/App.js
@@ -1,44 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import Article from "./components/Article";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Read from "./components/Read";
 import Create from "./components/Create";
 import Update from "./components/Update";
 import Control from "./components/Control";
+import { api_url } from "./Env";
 
 export default function App() {
   var [mode, setMode] = useState("WELCOME");
-  var [selectedId, setSelectedId] = useState(1);
+  var [selectedId, setSelectedId] = useState(null);
+  var [topic, setTopic] = useState();
   var [nextId, setNextId] = useState(4);
-  var [topics, setTopics] = useState([
-    { id: 1, title: "html", description: "html is..." },
-    { id: 2, title: "css", description: "css is..." },
-    { id: 3, title: "js", description: "js is..." }
-  ]);
+  var [topics, setTopics] = useState([]);
+  useEffect(function() {
+    getTopics(setTopics);
+  }, []);
+  function getTopics() {
+    fetch(api_url + "topics")
+      .then(function(type) {
+        return type.json();
+      })
+      .then(function(result) {
+        setTopics(result);
+      });
+  }
   var article = null;
   if (mode === "WELCOME") {
     article = <Article title="Welcome" description="Hello, WEB" />;
   } else if (mode === "READ") {
-    for (var i = 0; i < topics.length; i++) {
-      if (topics[i].id === selectedId) {
-        article = (
-          <Article
-            title={topics[i].title}
-            description={topics[i].description}
-          />
-        );
-      }
-    }
+    article = <Read id={selectedId} />;
   } else if (mode === "CREATE") {
     article = (
       <Create
         onCreate={function(data) {
-          data.id = nextId;
-          setTopics([...topics, data]);
-          setSelectedId(nextId);
-          setMode("READ");
-          setNextId(nextId + 1);
+          fetch(api_url + "topics", {
+            method: "POST",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopics(setTopics);
+              setSelectedId(result.id);
+              setMode("READ");
+            });
         }}
       />
     );
@@ -52,18 +67,26 @@ export default function App() {
     }
     article = (
       <Update
-        data={selectedTopic}
+        id={selectedId}
         onUpdate={function(data) {
-          var newTopics = [...topics];
-          for (var i = 0; i < newTopics.length; i++) {
-            if (newTopics[i].id === data.id) {
-              newTopics[i].title = data.title;
-              newTopics[i].description = data.description;
-              break;
+          fetch(api_url + "topics/" + data.id, {
+            method: "PUT",
+            body: JSON.stringify({
+              title: data.title,
+              description: data.description
+            }),
+            headers: {
+              "Content-Type": "application/json"
             }
-          }
-          setTopics(newTopics);
-          setMode("READ");
+          })
+            .then(function(type) {
+              return type.json();
+            })
+            .then(function(result) {
+              getTopics();
+              setSelectedId(result.id);
+              setMode("READ");
+            });
         }}
       />
     );
@@ -73,6 +96,7 @@ export default function App() {
       <Header
         onChangeMode={function() {
           setMode("WELCOME");
+          getTopics();
         }}
       />
       <Nav
@@ -86,19 +110,22 @@ export default function App() {
       <Control
         onChangeMode={function(mode) {
           if (mode === "DELETE") {
-            var newTopics = [];
-            for (var i = 0; i < topics.length; i++) {
-              if (topics[i].id === selectedId) {
-                continue;
-              } else {
-                newTopics.push(topics[i]);
+            fetch(api_url + "topics/" + selectedId, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json"
               }
-            }
-            console.log(newTopics);
-            setTopics(newTopics);
-            mode = "WELCOME";
+            })
+              .then(function(type) {
+                return type.json();
+              })
+              .then(function(result) {
+                setMode("WELCOME");
+                getTopics();
+              });
+          } else {
+            setMode(mode);
           }
-          setMode(mode);
         }}
       />
     </div>